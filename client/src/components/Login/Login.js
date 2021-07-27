fe440ee47b41b8253f717ecfabc7fe6024b82eca
Neil Creveling <neil.creveling@gmail.com>
2021-07-27 14:21:15 -0400

    add in PropType from new prop, destructure the props object to pull out setToken prop, create local state to capture Username and Password

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,18 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
 import './Login.css'; 
 
-export default function Login() {
+export default function Login({ setToken }) {
+  const [username, setUserName] = useState();
+  const [password, setPassword] = useState();
   return(
     <div className="login-wrapper">
     <h1>Please Log In</h1>
     <form>
       <label>
         <p>Username</p>
-        <input type="text" />
+        <input type="text" onChange={e => setUserName(e.target.value)}/>
       </label>
       <label>
         <p>Password</p>
-        <input type="password" />
+        <input type="password" onChange={e => setPassword(e.target.value)}/>
       </label>
       <div>
         <button type="submit">Submit</button>
@@ -20,4 +24,8 @@ export default function Login() {
     </form>
     </div>
   )
+}
+
+Login.propTypes = {
+  setToken: PropTypes.func.isRequired
 }
