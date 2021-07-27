70eb9539dfc4666209a64d3692eb87f16b53e036
Neil Creveling <neil.creveling@gmail.com>
2021-07-27 14:23:24 -0400

    coded post request to server, async function called loginUser, credentials as argument

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -3,6 +3,17 @@ import PropTypes from 'prop-types';
 
 import './Login.css'; 
 
+async function loginUser(credentials) {
+  return fetch('http://localhost:8080/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(credentials)
+  })
+    .then(data => data.json())
+ }
+
 export default function Login({ setToken }) {
   const [username, setUserName] = useState();
   const [password, setPassword] = useState();
